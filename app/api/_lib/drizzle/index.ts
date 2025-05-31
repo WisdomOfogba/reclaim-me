d4eb57302d4479558fdddcb614e6464a7480e07c
Wisdom Ofogba <wisetega007@gmail.com>
2025-05-31 14:53:16 +0100

    feat: export toolbarOptions in QuillEditorClient and comment out unused actionTypes in use-toast

--- app/api/_lib/drizzle/index.ts
+++ app/api/_lib/drizzle/index.ts
@@ -3,3 +3,4 @@ import postgres from "postgres"
 
 const client = postgres(process.env.DATABASE_URL!);
 const db = drizzle({ client });
+console.log(db, "Drizzle ORM initialized with PostgreSQL client");
